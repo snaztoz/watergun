1929eeb1d811f907c172363a0b2937544dd7de63
snaztoz <hafidhmn@gmail.com>
2025-12-18 14:40:13 +0700

    feat: create basic HTTP server

--- main.go
+++ main.go
@@ -1,7 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const port = "8080"
 
 func main() {
-	fmt.Println("Hello from WaterGun!")
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: handler(),
+	}
+
+	go func() {
+		slog.Info(fmt.Sprintf("Server is listening at port %s", port))
+
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("Server failed to listen at the specified port", "err", err)
+			return
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+
+	slog.Info("Shutting down the server...")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		slog.Error("Failed to gracefully shutting down the server", "err", err)
+		return
+	}
+
+	slog.Info("Server shutted down")
+}
+
+func handler() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", handleHealthCheck)
+	mux.HandleFunc("/live", handleWs)
+
+	return mux
+}
+
+func handleHealthCheck(w http.ResponseWriter, r *http.Request) {
+	if err := json.NewEncoder(w).Encode(map[string]any{
+		"message": "up",
+	}); err != nil {
+		slog.Error("Failed to send message", "err", err)
+	}
+}
+
+func handleWs(w http.ResponseWriter, r *http.Request) {
+	if err := json.NewEncoder(w).Encode(map[string]any{
+		"message": "upgrade to WebSocket",
+	}); err != nil {
+		slog.Error("Failed to send message", "err", err)
+	}
 }
